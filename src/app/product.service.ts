4679a866f41c1560031b0e0dc7f97c957816cf62
Yacinthos winner <yacintho@live.fr>
2020-10-13 16:34:00 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,10 +3,12 @@ import { Injectable } from '@angular/core';
 import {Http} from '@angular/http';
 import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
   constructor(private _http: Http) {
   }
 
@@ -14,5 +16,8 @@ export class ProductService {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 
+  public getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
+  }
 
 }
